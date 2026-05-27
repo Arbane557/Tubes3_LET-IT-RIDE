5b44bd2c38d7cfd82227cb125911171695261f41
Achideon <joshreinhartz@gmail.com>
2026-05-27 10:49:46 +0700

    feat: levenshtein without recursion
    i'm dping it uoooogh i'm dping it real hard

--- judol-detector/src/content/matching/fuzzy.ts
+++ judol-detector/src/content/matching/fuzzy.ts
@@ -1,28 +1,32 @@
 // using leventhein
 
-// can be optimised with dp (but idk ntar aja)
-function levenshtein(a: string, b: string, memo: Map<string, number> = new Map()): number {
-    if (b.length === 0) return a.length
-    if (a.length === 0) return b.length
-
-    const key1 = `${a}|${b}`
-    const key2 = `${b}|${a}`
-    if (memo.has(key1)) return memo.get(key1)!
-    else if (memo.has(key2)) return memo.get(key2)!
-
-    let result: number
-    if (a[0] === b[0]) {
-        result = levenshtein(a.slice(1), b.slice(1), memo)
-    } else {
-        result = 1 + Math.min(
-            levenshtein(a.slice(1), b, memo),
-            levenshtein(a, b.slice(1), memo),
-            levenshtein(a.slice(1), b.slice(1), memo)
-        )
+// i used this reference btw, goated material
+//  | | |
+//  | | |
+//  V V V
+// https://medium.com/@art3330/levenshtein-distance-fundamentals-817b6f7f1718
+function levenshtein(a: string, b: string): number {
+    let arr: number[][] = []
+    for(let i: number = 0; i <= a.length; i++){
+        arr[i] = []
+        for(let j: number = 0; j <= b.length; j++){
+            if(i === 0){
+                arr[i]?.push(j)
+            } else if (j === 0){
+                arr[i]?.push(i)
+            } else if (a[i-1] === b[j-1]) {
+                arr[i]?.push(arr.at(i-1)?.at(j-1)!)
+            } else {
+                let result: number = 1 + Math.min(
+                    arr.at(i-1)?.at(j-1)!,
+                    arr.at(i)?.at(j-1)!,
+                    arr.at(i-1)?.at(j)!
+                )
+                arr[i]?.push(result)
+            }
+        }
     } 
-    memo.set(key1, result)
-    memo.set(key2, result)
-    return result
+    return arr.at(a.length)?.at(b.length)!
 }
 
 export interface res {
@@ -57,7 +61,7 @@ export function fuzzy(text: string, pattern: string): res[] {
             if (textMemo.has(`${pattern}|${subtext}`)) d = textMemo.get(`${pattern}|${subtext}`)!
             else if (textMemo.has(`${subtext}|${pattern}`)) d = textMemo.get(`${subtext}|${pattern}`)!
             else{
-                d = levenshtein(pattern, subtext, textMemo)
+                d = levenshtein(pattern, subtext)
                 textMemo.set(`${pattern}|${subtext}`, d)
                 textMemo.set(`${subtext}|${pattern}`, d)
             }
